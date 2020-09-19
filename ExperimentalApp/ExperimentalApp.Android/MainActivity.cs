0030bfa6f791bececa670874a0c841a4a378cb92
Ryan Spryshak <ryan.spryshak@gmail.com>
2020-09-19 17:22:40 -0500

    Removed old solution because name was bad

--- ExperimentalApp/ExperimentalApp.Android/MainActivity.cs
+++ ExperimentalApp/ExperimentalApp.Android/MainActivity.cs
@@ -7,9 +7,9 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace App1.Droid
+namespace ExperimentalApp.Droid
 {
-    [Activity(Label = "App1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ExperimentalApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- ExperimentalApp/ExperimentalApp/MainPage.xaml.cs
+++ ExperimentalApp/ExperimentalApp/MainPage.xaml.cs
@@ -6,22 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace App1
+namespace ExperimentalApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            count++;
-            ((Button)sender).Text = $"You clicked {count} times.";
-        }
-
     }
 }
