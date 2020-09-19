43e13709642cba87cfc82a370203f76a0da059ea
Ryan Spryshak <ryan.spryshak@gmail.com>
2020-09-19 17:28:26 -0500

    Added back the counter

--- ExperimentalApp/ExperimentalApp/MainPage.xaml.cs
+++ ExperimentalApp/ExperimentalApp/MainPage.xaml.cs
@@ -10,9 +10,17 @@ namespace ExperimentalApp
 {
     public partial class MainPage : ContentPage
     {
+        int count = 0;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
         }
     }
 }
